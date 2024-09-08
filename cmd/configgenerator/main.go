f719d9811e9a91f89d74fbd7e4c465fc1c20b51a
j34sy <j34sy@proton.me>
2024-09-08 22:29:12 +0200

    feat: use writer functions

--- cmd/configgenerator/main.go
+++ cmd/configgenerator/main.go
@@ -2,161 +2,66 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/j34sy/configgenerator/pkg/devices"
 	"github.com/j34sy/configgenerator/pkg/importer"
 )
 
 func main() {
-	/*
-		fmt.Println("Will be implemented soon")
-
-		ipv4, err := datahandling.GetIPv4Address("192.168.100.12/24")
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		ipv4.Calculate()
-		fmt.Println(ipv4.GetNetworkAddress())
-
-		if len(os.Args) < 2 {
-			fmt.Println("Please provide a file path")
-			return
-		}
-
-		if len(os.Args) > 2 {
-			fmt.Println("Please provide only one file path")
-			return
-		}
-
-		filePath := os.Args[1]
-
-		networksYAML, err := importer.LoadYAML(filePath)
-
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-
-			for _, network := range *networksYAML {
-
-				importer.PrintFullNetworkInfoByYAML(&network)
-			}
-	*/
-
-	// Example usage
-
-	fullNetwork := []importer.NetworkYAML{
-		{
-			Routers: []importer.RouterYAML{
-				{
-					Name: "Router1",
-					Routes: importer.RoutesYAML{
-						Default:      "192.168.1.2",
-						Destinations: []string{"192.168.2.0/24", "192.168.3.0/24", "192.168.4.0/24", "192.168.5.0/24", "172.16.0.0/24", "192.168.100.0/24", "192.168.200.0/24"},
-					},
-					Interfaces: []importer.InterfaceYAML{
-						{Name: "eth0", IP: "192.168.1.1/24"},
-						{Name: "eth1", IP: "10.0.0.0/8"},
-					},
-				},
-				{
-					Name: "Router2",
-					Routes: importer.RoutesYAML{
-						Default:      "192.168.2.2",
-						Destinations: []string{"192.168.3.0/24", "192.168.4.0/24", "192.168.5.0/24", "10.0.0.0/8", "172.16.0.0/24", "192.168.100.0/24", "192.168.200.0/24"},
-					},
-					Interfaces: []importer.InterfaceYAML{
-						{Name: "eth0", IP: "192.168.2.1/24"},
-						{Name: "eth1", IP: "192.168.1.2/24"},
-					},
-				},
-				{
-					Name: "Router3",
-					Routes: importer.RoutesYAML{
-						Default:      "192.168.3.2",
-						Destinations: []string{"192.168.1.0/24", "192.168.4.0/24", "192.168.5.0/24", "10.0.0.0/8", "172.16.0.0/24", "192.168.100.0/24", "192.168.200.0/24"},
-					},
-					Interfaces: []importer.InterfaceYAML{
-						{Name: "eth0", IP: "192.168.3.1/24"},
-						{Name: "eth1", IP: "192.168.2.2/24"},
-					},
-				},
-				{
-					Name: "Router4",
-					Routes: importer.RoutesYAML{
-						Default:      "192.168.4.2",
-						Destinations: []string{"192.168.1.0/24", "192.168.2.0/24", "192.168.5.0/24", "10.0.0.0/8", "172.16.0.0/24", "192.168.100.0/24", "192.168.200.0/24"},
-					},
-					Interfaces: []importer.InterfaceYAML{
-						{Name: "eth0", IP: "192.168.4.1/24"},
-						{Name: "eth1", IP: "192.168.3.2/24"},
-					},
-				},
-			},
-			MLSwitches: []importer.MLSwitchYAML{
-				{
-					Name: "MLSwitch1",
-					Routes: importer.RoutesYAML{
-						Default:      "192.168.5.1",
-						Destinations: []string{"192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24", "10.0.0.0/8", "172.16.0.0/24", "192.168.100.0/24", "192.168.200.0/24"},
-					},
-					Interfaces: []importer.InterfaceYAML{
-						{Name: "eth0", IP: "192.168.4.2/24"},
-						{Name: "eth1", IP: "192.168.5.1/24"},
-					},
-					Routing: true,
-				},
-				{
-					Name: "MLSwitch2",
-					Routes: importer.RoutesYAML{
-						Default:      "",
-						Destinations: []string{"192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24", "192.168.4.0/24", "10.0.0.0/8", "192.168.100.0/24", "192.168.200.0/24"},
-					},
-					Interfaces: []importer.InterfaceYAML{
-						{Name: "eth0", IP: "192.168.5.2/24"},
-						{Name: "eth1", IP: "172.16.0.0/24"},
-					},
-					Routing: true,
-				},
-				{
-					Name: "MLSwitch3",
-					Routes: importer.RoutesYAML{
-						Default:      "192.168.4.2",
-						Destinations: []string{"192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24", "192.168.5.0/24", "10.0.0.0/8", "172.16.0.0/24", "192.168.200.0/24"},
-					},
-					Interfaces: []importer.InterfaceYAML{
-						{Name: "eth0", IP: "192.168.4.3/24"},
-						{Name: "eth1", IP: "192.168.100.1/24"},
-					},
-					Routing: true,
-				},
-				{
-					Name: "MLSwitch4",
-					Routes: importer.RoutesYAML{
-						Default:      "192.168.100.1",
-						Destinations: []string{"192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24", "192.168.4.0/24", "192.168.5.0/24", "10.0.0.0/8", "172.16.0.0/24"},
-					},
-					Interfaces: []importer.InterfaceYAML{
-						{Name: "eth0", IP: "192.168.200.1/24"},
-						{Name: "eth1", IP: "192.168.100.2/24"},
-					},
-					Routing: true,
-				},
-			},
-		},
+	// Check if the user provided a file path
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide a file path")
+		return
 	}
 
-	dest := "192.168.200.0/24"
-	nextHop := devices.FindNextHop(dest, devices.RoutingDevice{
-		Name: "Router3",
-		Interfaces: []devices.Interface{
-			{Name: "eth0", IP: "192.168.3.1/24"},
-			{Name: "eth1", IP: "192.168.2.2/24"},
-		},
-		Destinations: []string{""},
-		Default:      "",
-	}, &fullNetwork)
-	fmt.Println("Next Hop:", nextHop)
+	if len(os.Args) > 2 {
+		fmt.Println("Please provide only one file path")
+		return
+	}
+
+	filePath := os.Args[1]
+
+	networksYAML, err := importer.LoadYAML(filePath)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	networks := []Network{}
+
+	for _, network := range *networksYAML {
+
+		routers := []*devices.Router{}
+		switches := []*devices.Switch{}
+		mlSwitches := []*devices.MLSwitch{}
+
+		for _, routerYAML := range network.Routers {
+			routers = append(routers, devices.CreateRouter(routerYAML, network.Users, network.Name, networksYAML))
+		}
+
+		for _, switchYAML := range network.Switches {
+			switches = append(switches, devices.CreateSwitch(switchYAML, network.Users, network.Vlans, network.Name))
+		}
+
+		for _, mlSwitchYAML := range network.MLSwitches {
+			mlSwitches = append(mlSwitches, devices.CreateMLSwitch(mlSwitchYAML, network.Users, networksYAML, network.Name, network.Vlans))
+		}
+
+		networkData := Network{
+			Routers:    routers,
+			Switches:   switches,
+			MLSwitches: mlSwitches,
+		}
+
+		networks = append(networks, networkData)
+
+	}
+
+	for _, network := range networks {
+		fmt.Println("Network: ", network.Routers[0].Domain)
+		PrintNetwork(network)
+	}
+
 }
